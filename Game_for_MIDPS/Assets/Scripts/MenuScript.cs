b60d84f9a74afc9cfdcbe19d71429407c44ff216
StratanIon <95stratosfera@mail.ru>
2016-05-29 15:25:10 +0300

    Am creat Menu, cu meniul principal (2 butoane -> Play si Exit) , cu menu de iesire (2 butoane -> Yes si No), cu menu de alegere a levelui (6 butoane -> 1,2,3,4,5,6) care fiecare din ele deschide levele

---
+++ Game_for_MIDPS/Assets/Scripts/MenuScript.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class MenuScript : MonoBehaviour {
+
+    public GameObject levelChenger;
+    public GameObject exitPanel;
+
+    void Update()
+    {
+        if(levelChenger.activeSelf == true && Input.GetKeyDown(KeyCode.Escape))
+        {
+            levelChenger.SetActive(false);
+        }
+        else if (exitPanel.activeSelf ==false && Input.GetKeyDown(KeyCode.Escape))
+        {
+            exitPanel.SetActive(true);
+        }
+        else if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            exitPanel.SetActive(false);
+        }
+    }
+
+
+	public void OnClickStart()
+    {
+        levelChenger.SetActive(true);
+    }
+
+    public void OnClickExit()
+    {
+        Application.Quit();
+    }
+
+    public void levelButtons(int level)
+    {
+        SceneManager.LoadScene(level);
+    }
+}
